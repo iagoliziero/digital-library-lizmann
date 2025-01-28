561088eb7f0273fad06ef9680ad059e36451de97
Liziero <iagolizierox14@gmail.com>
2025-01-28 12:56:24 -0300

    Terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import Biblioteca from "./pages/Biblioteca";
 
-
-
 function App() {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      UrlDaImagem: "",
+      title: "O Poder do Hábito",
+      autor: "Charles Duhigg",
+      ano: 2012,
+      avaliacao: 4,
+    },
+    {
+      id: 2,
+      UrlDaImagem: "",
+      title: "Hábitos Atômicos",
+      autor: "James Clear",
+      ano: 2018,
+      avaliacao: 5,
+    },
+    {
+      id: 3,
+      UrlDaImagem: "",
+      title: "Hábitos Atômicos",
+      autor: "James Clear",
+      ano: 2019,
+      avaliacao: 5,
+    },
+  ]);
       return (
-        <Biblioteca />
+        <Biblioteca books={books} />
       )
 }
 export default App;
--- src/pages/Biblioteca.jsx
+++ src/pages/Biblioteca.jsx
@@ -1,38 +1,51 @@
-import { BookOpenText, Pencil, Trash2 } from "lucide-react";
+import { BookOpenText, Menu, Pencil, Trash2 } from "lucide-react";
 
-function Biblioteca() {
+
+function Biblioteca({books}) {
   return (
     <div>
-      <div className="h-screen bg-gray-200 bg-cover bg-center bg-no-repeat flex justify-center">
+      <div className="min-h-screen flex justify-center bg-gray-200 overflow-hidden">
         <div>
-          <div className="flex justify-center gap-1">
+          <div className="flex justify-center space-x-20">
             <img
-              className="w-[100px] md:w-[125px] mt-8"
+              className="w-[100px] md:w-[125px] mt-8 relative left-14 md:left-16"
               src="src/assets/logo-lizmann.png"
               alt="logo lizmann"
             />
+            <div className=" flex items-center justify-end relative left-7 md:left-32">
+              <button>
+                <Menu size={44} strokeWidth={2.75} />
+              </button>
+            </div>
           </div>
           {/* cards */}
-          <div>
-            <div
-             className="flex w-[250px] space-y-4 h-[475px] border-solid border-2 rounded-2xl border-zinc-500 flex-col items-center  text-center mt-[125px] bg-slate-100">
-              <div
-               className="flex w-[200px] space-y-4 h-[200px]  rounded-2xl flex-col items-center text-center mt-[20px] bg-gray-300"></div>
-              <div className="flex flex-col gap-2">
-                <h1 className="text-2xl font-semibold"> Nome do livro </h1>
-                <h2 className="text-xl flex "> Autor </h2>
-                <h2 className="text-xl flex mb-1"> 2025 </h2>
-                <h2 className="flex gap-1"> <BookOpenText /> <BookOpenText /> <BookOpenText /> <BookOpenText /> <BookOpenText /></h2>
-                <div className="flex mt-3 space-x-5">
-                  <button className="w-14 bg-brown text-slate-100 p-4 h-14 rounded-lg hover:scale-105 ease-out duration-300">
-                  <Trash2 />
-                  </button>
-                  <button className="w-14 bg-brown text-slate-100 p-4 h-14 rounded-lg hover:scale-105 ease-out duration-300">
-                  <Pencil/>
-                  </button>
+          <div className="flex gap-4">
+            {books.map((books) => (
+              
+              <div key={books.id}
+              className="flex w-[260px] space-y-4 h-[475px] border-solid border-2 rounded-2xl border-zinc-500 flex-col items-center  text-center mt-[125px] bg-slate-100">
+                <div className="flex w-[200px] space-y-4 h-[200px]  rounded-2xl flex-col items-center text-center mt-[20px] bg-gray-300"></div>
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-2xl font-semibold">
+                    {books.title}  </h1>
+                  <h2 className="text-xl flex "> {books.autor} </h2>
+                  <h2 className="text-xl flex mb-1"> {books.ano} </h2>
+                  <h2 className="flex gap-1">
+                    {" "}
+                    <BookOpenText /> <BookOpenText /> <BookOpenText />{" "}
+                    <BookOpenText /> <BookOpenText />
+                  </h2>
+                  <div className="flex mt-3 space-x-9">
+                    <button className="w-14 bg-brown text-slate-100 p-4 h-14 rounded-lg hover:scale-105 ease-out duration-300">
+                      <Trash2 />
+                    </button>
+                    <button className="w-14 bg-brown text-slate-100 p-4 h-14 rounded-lg hover:scale-105 ease-out duration-300">
+                      <Pencil />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            ) )}
           </div>
           {/* cards */}
         </div>
