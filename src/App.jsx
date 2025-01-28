792281c2c9245aa9dee4a8d75bb62aa2906a9501
Liziero <iagolizierox14@gmail.com>
2025-01-28 18:43:12 -0300

    Quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,39 @@ import Biblioteca from "./pages/Biblioteca";
 
 function App() {
   const [books, setBooks] = useState([
-    {
-      id: 1,
-      UrlDaImagem: "",
-      title: "O Poder do Hábito",
-      autor: "Charles Duhigg",
-      ano: 2012,
-      avaliacao: 4,
-    },
-    {
-      id: 2,
-      UrlDaImagem: "",
-      title: "Hábitos Atômicos",
-      autor: "James Clear",
-      ano: 2018,
-      avaliacao: 5,
-    },
-    {
-      id: 3,
-      UrlDaImagem: "",
-      title: "Hábitos Atômicos",
-      autor: "James Clear",
-      ano: 2019,
-      avaliacao: 5,
-    },
+     {
+    id: 1,
+    title: "O Poder do Hábito",
+    autor: "Charles Duhigg",
+    ano: 2012,
+    genero: "Autoajuda",
+    imagem: "https://m.media-amazon.com/images/I/815iPX0SgkL.jpg",
+  },
+  {
+    id: 2,
+    title: "1984",
+    autor: "George Orwell",
+    ano: 1949,
+    genero: "Ficção Distópica",
+    imagem: "https://m.media-amazon.com/images/I/819js3EQwbL.jpg",
+  },
+  {
+    id: 3,
+    title: "Dom Casmurro",
+    autor: "Machado de Assis",
+    ano: 1899,
+    genero: "Literatura Brasileira",
+    imagem: "https://m.media-amazon.com/images/I/61Z2bMhGicL.jpg",
+  },
+  {
+    id: 4,
+    title: "Can't Hurt Me",
+    autor: "David Goggins",
+    ano: 2018,
+    genero: "Autobiografia / Motivação",
+    imagem: "https://m.media-amazon.com/images/I/710oCZaYYUL.jpg",
+  },
+  
   ]);
       return (
         <Biblioteca books={books} />
--- src/pages/Biblioteca.jsx
+++ src/pages/Biblioteca.jsx
@@ -4,7 +4,7 @@ import { BookOpenText, Menu, Pencil, Trash2 } from "lucide-react";
 function Biblioteca({books}) {
   return (
     <div>
-      <div className="min-h-screen flex justify-center bg-gray-200 overflow-hidden">
+      <div className="min-h-screen flex justify-center bg-gray-200 overflow-">
         <div>
           <div className="flex justify-center space-x-20">
             <img
@@ -24,16 +24,14 @@ function Biblioteca({books}) {
               
               <div key={books.id}
               className="flex w-[260px] space-y-4 h-[475px] border-solid border-2 rounded-2xl border-zinc-500 flex-col items-center  text-center mt-[125px] bg-slate-100">
-                <div className="flex w-[200px] space-y-4 h-[200px]  rounded-2xl flex-col items-center text-center mt-[20px] bg-gray-300"></div>
+                <div className="flex space-y-4 h-[200px]  rounded-2xl flex-col items-center text-center mt-[20px]"> <img className="max-h-[200px] rounded-lg" src={books.imagem} alt="Imagem livro" /></div>
                 <div className="flex flex-col gap-2">
                   <h1 className="text-2xl font-semibold">
                     {books.title}  </h1>
                   <h2 className="text-xl flex "> {books.autor} </h2>
                   <h2 className="text-xl flex mb-1"> {books.ano} </h2>
-                  <h2 className="flex gap-1">
-                    {" "}
-                    <BookOpenText /> <BookOpenText /> <BookOpenText />{" "}
-                    <BookOpenText /> <BookOpenText />
+                  <h2 className="text-xl flex">
+                    {books.genero}
                   </h2>
                   <div className="flex mt-3 space-x-9">
                     <button className="w-14 bg-brown text-slate-100 p-4 h-14 rounded-lg hover:scale-105 ease-out duration-300">
